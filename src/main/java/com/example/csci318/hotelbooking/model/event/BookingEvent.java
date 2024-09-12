fb477acdff1963a21ea554803a5d479e21997357
khanhsaan <anthonykhanhan@gmail.com>
2024-09-12 20:30:04 +1000

    Create more attributes for BookingEvent, enable h2 console and add data source url

--- src/main/java/com/example/csci318/hotelbooking/model/event/BookingEvent.java
+++ src/main/java/com/example/csci318/hotelbooking/model/event/BookingEvent.java
@@ -1,5 +1,15 @@
 package com.example.csci318.hotelbooking.model.event;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class BookingEvent {
-    int test;
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column
+    private String eventName;
 }
