8e4fc4364000566b660d82d47b31983713f7ef36
khanhsaan <anthonykhanhan@gmail.com>
2024-09-13 00:08:20 +1000

    Create HotelEvent

---
+++ src/main/java/com/example/csci318/hotelbooking/model/event/HotelEvent.java
@@ -0,0 +1,66 @@
+package com.example.csci318.hotelbooking.model.event;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class HotelEvent {
+    @Id
+    @GeneratedValue
+    private long Id;
+    @Column
+    private String name;
+    @Column
+    private String location;
+    @Column
+    private String description;
+    @Column
+    private double pricePerNight;
+
+    public HotelEvent() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    @Override
+    public String toString() {
+        return "HotelEvent{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                ", pricePerNight=" + pricePerNight +
+                '}';
+    }
+}
