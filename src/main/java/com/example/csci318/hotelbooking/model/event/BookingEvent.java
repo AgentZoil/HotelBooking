17feee1136e4e56e696dc9277fdc45ec6feb7169
khanhsaan <anthonykhanhan@gmail.com>
2024-09-13 00:05:58 +1000

    Create BookingEvent

--- src/main/java/com/example/csci318/hotelbooking/model/event/BookingEvent.java
+++ src/main/java/com/example/csci318/hotelbooking/model/event/BookingEvent.java
@@ -1,9 +1,11 @@
 package com.example.csci318.hotelbooking.model.event;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.csci318.hotelbooking.model.Hotel;
+import com.example.csci318.hotelbooking.model.Room;
+import com.example.csci318.hotelbooking.model.User;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class BookingEvent {
@@ -12,4 +14,77 @@ public class BookingEvent {
     private long id;
     @Column
     private String eventName;
+    @Column
+    private User user;
+    @Column
+    private Hotel hotel;
+    @Column
+    private Room room;
+    @Column
+    private Date checkInDate;
+    @Column
+    private Date checkOutDate;
+
+    public BookingEvent(){
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingEvent{" +
+                "eventName='" + eventName + '\'' +
+                ", user=" + user +
+                ", hotel=" + hotel +
+                ", room=" + room +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                '}';
+    }
 }
