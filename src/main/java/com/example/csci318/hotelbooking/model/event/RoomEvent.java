5547360cd97b9f0ce97389d86557a50f3ce12b96
khanhsaan <anthonykhanhan@gmail.com>
2024-09-15 12:58:00 +1000

    Fix @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/com/example/csci318/hotelbooking/model/event/RoomEvent.java
+++ src/main/java/com/example/csci318/hotelbooking/model/event/RoomEvent.java
@@ -1,14 +1,11 @@
 package com.example.csci318.hotelbooking.model.event;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class RoomEvent {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
     @Column
     private String eventName;
--- src/main/java/com/example/csci318/hotelbooking/model/event/UserEvent.java
+++ src/main/java/com/example/csci318/hotelbooking/model/event/UserEvent.java
@@ -1,14 +1,11 @@
 package com.example.csci318.hotelbooking.model.event;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class UserEvent {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column
     private String eventName;
